5f70fc743b5f250249f8a7f3b0425470dbfe0230
jeswinjoseph123 <jeswinjoseph08@gmail.com>
2025-12-02 18:30:00 +0000

    CURD apis added

--- Day_9_Intro_to_Backend/backend/src/app.js
+++ Day_9_Intro_to_Backend/backend/src/app.js
@@ -6,12 +6,12 @@ app.use(express.json());
 
 //routes import
 import userRouter from "./routes/user.route.js";
-// import postRouter from "./routes/post.route.js";
+import postRouter from "./routes/post.route.js";
 
 //route declaration
 
 app.use("/api/v1/users", userRouter);
-// app.use("/api/v1/posts", postRouter);
+app.use("/api/v1/posts", postRouter);
 
 //ex router : http://localhost:4000/api/v1/users/register
 
---
+++ Day_9_Intro_to_Backend/backend/src/controllers/post.controller.js
@@ -0,0 +1,85 @@
+import { Post } from "../models/post.model.js";
+
+const createPost = async (req, res) => {
+  const { name, description, age } = req.body;
+  if (!name || !description || !age) {
+    return res.status(400).json({
+      message: "All feilds are required",
+    });
+  }
+  const post = await Post.create({
+    name,
+    description,
+    age,
+  });
+  res.status(201).json({
+    message: "Post created successfully",
+    post,
+  });
+
+  try {
+  } catch (error) {
+    res.status(500).json({
+      message: "Internal server error",
+      error,
+    });
+  }
+};
+
+const getPosts = async (req, res) => {
+  try {
+    const posts = await Post.find();
+    res.status(200).json(posts);
+  } catch (error) {
+    res.status(500).json({
+      message: "Internal server error",
+      error,
+    });
+  }
+};
+
+const updatePost = async (req, res) => {
+  try {
+    if (Object.keys(req.body).length === 0) {
+      return res.status(400).json({
+        message: "No data is Provided",
+      });
+    }
+    const post = await Post.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+    });
+    if (!post)
+      return res.status(400).json({
+        message: "post not found",
+      });
+    res.status(200).json({
+      message: "Post updated Successfully",
+      post,
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: "Internal server error",
+      error,
+    });
+  }
+};
+
+const deletePost = async (req, res) => {
+  try {
+    const deleted = await Post.findByIdAndDelete(req.params.id);
+    if (!deleted)
+      return res.status(404).json({
+        message: "Post not found",
+      });
+    res.status(200).json({
+      message: "Post deleted succesfully",
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: "Internal server error",
+      error,
+    });
+  }
+};
+
+export { createPost, getPosts, updatePost, deletePost };
