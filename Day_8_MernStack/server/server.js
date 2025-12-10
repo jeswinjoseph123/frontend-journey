af5be4175072670d10397cfc4125162e974acca5
jeswinjoseph123 <jeswinjoseph08@gmail.com>
2025-12-10 19:34:27 +0000

    some backend code

---
+++ Day_8_MernStack/server/server.js
@@ -0,0 +1,34 @@
+import express from "express";
+import mongoose from "mongoose";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+
+mongoose
+  .connect(
+    "mongodb+srv://jeswinjoseph08_db_user:wv7nGyAvD3NaxvPB@cluster0.fwkpobz.mongodb.net/"
+  )
+  .then(() => console.log("MongoDB connected"))
+  .catch((error) => console.log(error));
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST", "DELETE", "PUT"],
+    allowedHeaders: [
+      "Content-Type",
+      "Authorization",
+      "Cache-Control",
+      "Expires",
+      "Pragma",
+    ],
+    credentials: true,
+  })
+);
+
+app.use(cookieParser());
+app.use(express.json());
+
+app.listen(PORT, () => console.log(`Server is running in port : ${PORT}`));
